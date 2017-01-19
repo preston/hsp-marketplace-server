3f776365c7c1c998489b7b60e500098ed82a3995
Preston Lee <preston.lee@prestonlee.com>
2017-01-19 16:25:10 -0700

    Image processing support; general cleanup.

---
+++ db/migrate/20170119211552_create_screenshot_images.rb
@@ -0,0 +1,18 @@
+class CreateScreenshotImages < ActiveRecord::Migration
+    def self.up
+        create_table :images, id: :uuid do |t|
+            t.uuid :screenshot_id, null: false
+            t.string     :style
+            t.binary     :file_contents
+
+            t.timestamps
+        end
+		add_index :images, :screenshot_id
+		add_foreign_key :images, :screenshots, on_delete: :cascade
+
+    end
+
+    def self.down
+        drop_table :images
+    end
+end
