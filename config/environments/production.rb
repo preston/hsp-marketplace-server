be83d69fdc9743a9a09f0857318501d922388855
Preston Lee <preston.lee@prestonlee.com>
2017-03-27 23:42:42 -0500

    Demo fix.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,9 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  # TODO This seems bad. Is it really a reasonable default for containerized builds?
+  config.action_cable.disable_request_forgery_protection = true
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
