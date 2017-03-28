00623436233e2b9e8a69fc7123eccf3021d6d21d
Preston Lee <preston.lee@prestonlee.com>
2017-03-27 20:28:44 -0500

    Message format fix.

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -5,29 +5,35 @@ class Instance < ApplicationRecord
     validates_presence_of :platform
     validates_presence_of :build
 
-    after_create    :notify_create
+    after_create :notify_create
 
     def notify_create
-        PlatformChannel.broadcast_to self.platform, {
+        PlatformChannel.broadcast_to self.platform,
             resource_type: 'instance',
-            event_type: "created",
-            message: 'A build instance has been added to the platform.',
-            model: {
-                build: {
-                    id: self.build.id,
-                    container_repository: self.build.container_repository,
-                    container_tag: self.build.container_tag,
-                    service: {
-                        id: self.build.service.id
-                    }
-                },
-                platform: {
-                    id: self.platform.id,
-                    user: {
-                        id: self.platform.user.id
-                    }
-                }
-            }
-        }
+           event_type: 'created',
+           message: 'A build instance has been added to the platform.',
+           model: {
+               instance: {
+                   build: {
+                       id: build.id,
+                       container_repository: build.container_repository,
+                       container_tag: build.container_tag,
+                       service: {
+                           id: build.service.id
+                       }
+                   },
+                   platform: {
+                       id: platform.id,
+                       user: {
+                           id: platform.user.id
+                       }
+                   }
+               }
+           }
     end
+
+    def force_notify_create
+        self.notify_create
+    end
+
 end
