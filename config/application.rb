c5ce7cba8679a847e6937037a9b6724d98c3a039
Preston Lee <preston.lee@prestonlee.com>
2017-05-30 16:23:57 -0700

    Completing Rails v5.1 upgrade.

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
@@ -8,8 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Marketplace
   class Application < Rails::Application
-	  # Settings in config/environments/* take precedence over those specified here.
-      # Application configuration should go into files in config/initializers
-      # -- all .rb files in that directory are automatically loaded.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
